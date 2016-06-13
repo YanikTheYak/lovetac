6659ac540fbd1a82663da1a29a2c88ba01bc9d3c
Guillaume Gojard <ggojard@gmail.com>
2016-06-13 16:12:25 +0200

    change root page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
  // include_once("home.html");
- header('Location: /public/index.html');
+ header('Location: /public/dashboard.html');
  ?>
--- public/js/main.js
+++ public/js/main.js
@@ -28,17 +28,6 @@ function downloadJSON(obj) {
   return ('<a class="download" href="data:' + data + '" download="'+filename+'.json">Download (.json)</a>');
 }
 
-// -------------- //
-// Date.prototype.yyyymmdd = function() {
-//    var yyyy = this.getFullYear().toString();
-//    var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
-//    var dd = this.getDate().toString();
-//    return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
-//   };
-
-
-
-
 // function ...
 
 function displayExcelImport() {
@@ -66,11 +55,11 @@ function main() {
     });
 
 
-    // $(".excel2Tac").hide();
+    $(".excel2Tac").hide();
     // $(".evo2tac").hide();
 
     // ********** LOAD from EVOLVE *****************
-    // loadContent(data, dic, enableD3JS);
+    loadContent(data, dic, enableD3JS);
 
     // ********** APIs *****************
     // getAuthenTac();
