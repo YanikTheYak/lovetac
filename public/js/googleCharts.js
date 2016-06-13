bba822068c49d715bdba35ff5512a566d70bc5ae
Guillaume Gojard <ggojard@gmail.com>
2016-06-13 15:33:21 +0200

    add files to deploy on heroku 12

--- public/js/googleCharts.js
+++ public/js/googleCharts.js
@@ -4,10 +4,10 @@ google.charts.load('current', {
   'packages': ['corechart']
 });
 
-// Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart_numberOfJobsPerIs);
-// google.charts.setOnLoadCallback(drawChart_averageDurationOfJobs);
-google.charts.setOnLoadCallback(drawChart_ColumnChart);
+// Set a callback to run when the Google Visualization API is loaded:
+  google.charts.setOnLoadCallback(drawChart_numberOfJobsPerIs);
+  // google.charts.setOnLoadCallback(drawChart_averageDurationOfJobs);
+  google.charts.setOnLoadCallback(drawChart_ColumnChart);
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
